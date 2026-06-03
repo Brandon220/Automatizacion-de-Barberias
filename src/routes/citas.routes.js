1c42c55bc4c697e744fe0c9085c43979afca7edd
Brandon220 <brandongarcia20052313@gmail.com>
2026-06-03 02:35:44 -0600

    fix modelos minuscula

--- src/routes/citas.routes.js
+++ src/routes/citas.routes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const Cita = require("../models/Cita");
+const Cita = require('../models/cita');
 const enviarNotificacion = require("../utils/notify");
 
 // ⏱️ Duración por servicio
